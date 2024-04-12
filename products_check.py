caf1fd901d42635afb69073b991ba413d7cf7447
MeeraRao3 <92389935+MeeraRao3@users.noreply.github.com>
2024-04-12 23:35:22 +0530

    Update products_check.py

--- products_check.py
+++ products_check.py
@@ -1,63 +1,3 @@
-# import csv
-# import requests
-
-# # Replace 'YOUR_TOKEN' with your actual API token
-# API_TOKEN = 'acefdc2e8e7611f4997c680d7bb9992637380e37f9b5f07d9af4bd08c35de161'
-
-# # G2 API endpoint for products
-# API_ENDPOINT = 'https://data.g2.com/api/v1/products'
-
-# def check_product_availability(product_name):
-#     headers = {
-#         'Authorization': f'Bearer {API_TOKEN}'
-#     }
-#     params = {
-#         'filter[name]': product_name
-#     }
-#     response = requests.get(API_ENDPOINT, headers=headers, params=params)
-#     if response.status_code == 200:
-#         data = response.json()
-#         if 'data' in data:
-#             products = data['data']
-#             if products:
-#                 # Check if the product we're interested in is listed
-#                 for product in products:
-#                     if 'attributes' in product and 'name' in product['attributes'] and product['attributes']['name'] == product_name:
-#                         return True
-#             else:
-#                 return False
-#         else:
-#             # Handle missing 'data' key in response
-#             print('Error: No data found in response')
-#             return None
-#     else:
-#         # Handle API error
-#         print('Error:', response.status_code)
-#         return None
-
-# def check_products_in_csv(csv_file):
-#     with open(csv_file, 'r') as file:
-#         reader = csv.reader(file)
-#         next(reader)  # Skip header row if present
-#         for row in reader:
-#             product_name = row[0]  # Assuming the product name is in the first column
-#             is_listed = check_product_availability(product_name)
-#             if is_listed is None:
-#                 print(f'Failed to check availability for {product_name}.')
-#             elif is_listed:
-#                 print(f'{product_name} is listed on G2.')
-#             else:
-#                 print(f'{product_name} is not listed on G2.')
-
-
-# def main():
-#     # Example CSV file containing product names
-#     csv_file = '/Users/mohanrao/Downloads/products1.csv'
-#     check_products_in_csv(csv_file)
-
-# if __name__ == "__main__":
-#     main()
-
 import csv
 import requests
 
@@ -101,7 +41,7 @@ def check_products_in_csv(csv_file):
         reader = csv.reader(file)
         next(reader)  # Skip header row if present
         for row in reader:
-            product_name = row[0]  # Assuming the product name is in the first column
+            product_name = row[0]  
             is_listed = check_product_availability(product_name)
             if is_listed is None:
                 print(f'Failed to check availability for {product_name}.')
